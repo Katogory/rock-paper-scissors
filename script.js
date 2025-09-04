238d9e81877485aebedf44e72ed0e44744da62e7
Katogory <145012865+Katogory@users.noreply.github.com>
2025-09-04 20:02:36 +0400

    feat: add playGame function for multiple rounds

--- script.js
+++ script.js
@@ -16,16 +16,12 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors?");
-    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors"){
-        humanChoice = prompt("Please choose Rock Paper or Scissors");
+    let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
+        humanChoice = prompt("Please choose Rock Paper or Scissors").toLowerCase();
     }
-    let humanChoiceLower = humanChoice.toLowerCase();
-    return humanChoiceLower;
-    }
-
-let humanScore = 0;
-let computerScore = 0;
+    return humanChoice;
+}
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
@@ -33,12 +29,40 @@ function playRound(humanChoice, computerChoice) {
     } else if ((humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper") ||
         (humanChoice === "rock" && computerChoice === "scissors")) {
-        humanScore++;
-        return "player wins!";
+        return "Player wins!";
     } else {
-        computerScore++;
         return "Computer wins!";
     }
 }
 
-console.log(playRound(getHumanChoice(), getComputerChoice()));
+function playGame() {
+    let roundAmount = parseInt(prompt("How many rounds?"), 10);
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < roundAmount; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        const result = playRound(humanChoice, computerChoice);
+        if (result === "It's a tie!"){
+            console.log("Tie round!")
+            console.log(`Player ${humanScore} --- Computer ${computerScore}`)
+        }else if (result === "Player wins!"){
+            humanScore++
+            console.log("Human wins the round!")
+            console.log(`Player ${humanScore} --- Computer ${computerScore}`)
+        }else {
+            computerScore++
+            console.log("Computer wins the round!")
+            console.log(`Player ${humanScore} --- Computer ${computerScore}`)            
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log("Human wins the game!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer wins the game!");
+    } else {
+        console.log("It's a tie game!");
+    }
+}
+
+playGame(5);
